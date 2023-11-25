13794c5d934d4fedfbb23c9ee7385502ebe1f4c2
moerer-man <152007733+moerer-man@users.noreply.github.com>
2023-11-25 08:17:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add any JavaScript functionality or interactivity here
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Add logic to handle form submission
+    // For example, you can use AJAX to send form data to a server
+    alert('Form submitted!');
+});
