e6254671bebba9933fffbb57eb0baa8fd225a5e6
moerer-man <152007733+moerer-man@users.noreply.github.com>
2024-03-19 13:39:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-<script>
-        // Smooth scrolling when clicking on links
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-                e.preventDefault();
-                
-                const sectionId = this.getAttribute('href');
-                document.querySelector(sectionId).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
-    </script>
-        // Add any JavaScript functionality or interactivity here
+// Add any JavaScript functionality or interactivity here
 
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
